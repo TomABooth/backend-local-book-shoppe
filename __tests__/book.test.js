23bf5c28775caa7a48928e239225001f10788f8f
Tom Booth <tombeezle@gmail.com>
2022-11-08 14:25:41 -0800

    added tests, forgot to commit, then added models and controller and first test passes

---
+++ __tests__/book.test.js
@@ -0,0 +1,18 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('book routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(7);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,37 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    dob DATE,
+    pob VARCHAR
+);
+
+INSERT INTO
+    authors (name, dob, pob)
+VALUES
+    ('Douglas Adams', '1952-03-11', 'Cambridge'),
+    ('C.S. Lewis', '1898-11-29', 'Oxford'),
+    ('J.R.R. Tolkien', '1892-01-03', 'Bloemfontein'),
+    ('Ray Bradbury', '1920-08-22', 'Waukegan'),
+    ('George Orwell', '1903-06-25', 'Motifari');
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    released INT NOT NULL
+);
+
+INSERT INTO
+    books (title, released)
+VALUES
+    ('Hitchhikers Guide to the Galaxy', 1979),
+    ('The Restaurant at the End of the Universe', 1980),
+    ('The Lion, the Witch and the Wardrobe', 1950),
+    ('The Hobbit', 1937),
+    ('The Fellowship of the Ring', 1954),
+    ('Fahrenheit 451', 1953),
+    ('Nineteen Eighty-Four', 1949);
